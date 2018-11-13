a6b9797c851a4d4ccf14e29030441bfefad96ccc
cutshadows <ramnstein_1749_@hotmail.com>
2018-11-13 17:29:00 -0300

    trabajando en admin

---
+++ bdaldunate.sql
@@ -0,0 +1,18 @@
+﻿# Host: localhost  (Version 5.5.5-10.1.36-MariaDB)
+# Date: 2018-11-13 17:27:32
+# Generator: MySQL-Front 6.1  (Build 1.26)
+
+
+#
+# Structure for table "tb_producto"
+#
+
+CREATE TABLE `tb_producto` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "tb_producto"
+#
+
