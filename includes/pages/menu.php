4d4e654ee6a2afb70558daa565cf9170556fc7e5
cutshadows <ramnstein_1749_@hotmail.com>
2018-11-15 16:07:44 -0300

    modificacion de resolucion 4k

--- includes/pages/menu.php
+++ includes/pages/menu.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="#"> <img src="img/aldunate-logo.jpg" width="20%;" alt="" srcset=""> </a>
+      <a class="navbar-brand" href="#"> <img src="img/logo_aldunate.png" width="20%;" alt="" srcset=""> </a>
       <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="navbar-collapse collapse" id="navbarsExampleDefault" style="color-background:#9c9c9c!important;">
+      <div class="navbar-collapse collapse" id="navbarsExampleDefault" >
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
